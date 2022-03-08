6daee629e9e90bb7723e7820ac311fcb08554fb1
cmsjulio <cms.julio1@gmail.com>
2022-03-08 13:53:55 -0300

    Sobre o básico de component.

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  selector: 'app-test', //can be stated and called in other ways
+  templateUrl: './test.component.html', //can be used as 'template'
+  styleUrls: ['./test.component.scss'] //pointing to the css config, can be called directly by using 'style'
 })
 export class TestComponent implements OnInit {
 
