538557612eb81bdbb0f8e5e94d3cb9f86c750b0a
cmsjulio <cms.julio1@gmail.com>
2022-04-27 14:09:24 -0300

    8 - Style Binding

--- hello-world/src/app/test2/test2.component.ts
+++ hello-world/src/app/test2/test2.component.ts
@@ -10,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
     <h2 [class] = "successClass">Codevolution</h2>
     <h2 [class.text-danger]="hasError">Codevolution</h2>
 
-    <h2 [ngClass]="messageClasses">Codevolution</h2>
+    <h2 [ngClass]="messageClasses">CSS class binding</h2>
+
+    <h2 [style.color]="'orange'">Utiliza CSS properties após o 'style.' para editar css direto no HTML</h2>
+
+    <h2 [style.color]="hasError ? 'red' : 'green' ">Tem erro? Se sim, red. Se não, verde. Ternário. </h2>
+
+    <h2 [style.color]="highlightColor"> Binding com style e propriedade typescript. </h2>
+
+    <h2 [ngStyle]="titleStyles"> Binding com ngStyle. </h2>
   `,
   styles: [`
     .text-success {
@@ -39,6 +47,13 @@ export class Test2Component implements OnInit {
     "text-danger": this.hasError,
     "text-special": this.isSpecial
   }
+
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+  public highlightColor = "orange";
 
   constructor() { }
 
